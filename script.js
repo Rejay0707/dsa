86616e61b458ef41db47ff655e9738ae1e6481da
Rejay0707 <rejaysobis15@gmail.com>
2024-06-13 12:23:24 +0530

    completed fibinocii series

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ const isAnagram = function (s, t) {
 };
 
 isAnagram("anagram", "nagaram");
+
+//fibinocii series
+
+const fib = function (n) {
+    const arr = [0, 1];
+  
+    for (let i = 2; i <= n; i++) {
+      arr.push(arr[i - 1] + arr[i - 2]);
+      console.log(arr[i]);
+    }
+    console.log(arr[n]);
+  };
+  
+  fib(5);
+
+  
